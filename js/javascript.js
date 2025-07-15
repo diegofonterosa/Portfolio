f8d264cc48362b831109df83f70ce781efc2a362
diego <diegofonterosa@gmail.com>
2025-07-15 15:08:40 +0200

    Inicio proyecto + cookies 15/07

---
+++ js/javascript.js
@@ -0,0 +1,12 @@
+//Política de cookies
+const popup = document.getElementById("cookiePopup");
+const btn = document.getElementById("acceptCookies");
+
+if(!localStorage.cookiesAccepted){
+    popup.classList.add("show");
+}
+
+btn.addEventListener("click", function(){
+    localStorage.cookiesAccepted = "true";
+    popup.classList.remove("show");
+});
