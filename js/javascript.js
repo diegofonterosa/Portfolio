c3a4bec0742edd3b845be66e349172f408aef4f9
diego <diegofonterosa@gmail.com>
2025-07-16 19:37:42 +0200

    Menu hamburguesa 16/07

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,30 @@
+// Menú hamburguesa
+const hamburguer = document.getElementById('hamburguer');
+const navMenu = document.getElementById('nav-menu');
+
+hamburguer.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+    })
+
+document.querySelectorAll('.nav-menu a').forEach(link => {
+    link.addEventListener('click', () => {
+        navMenu.classList.remove('active')
+    })
+})
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
+
 //Política de cookies
 const popup = document.getElementById("cookiePopup");
 const acceptBtn = document.getElementById("acceptCookies");
